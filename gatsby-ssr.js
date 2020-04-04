e0024c38ebd98d9f0f4494f92ce48a52e1287980
Ville Heikkilä <tehomail@gmail.com>
2020-04-05 00:35:41 +0300

    feat: include fonts and colors in the theme

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,6 +3,10 @@ import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
 import './src/global.css';
 
+require('typeface-open-sans');
+require('typeface-quicksand');
+require('typeface-merriweather');
+
 export const wrapRootElement = ({ element }) => {
     return <ThemeProvider theme={theme}>{element}</ThemeProvider>;
 };
--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,35 @@
 export default {
     colors: {
-        black: '#000000',
-        white: '#FFFFFF',
+        smokyBlack: '#090c08',
+        outerSpace: '#444545',
+        onyx: '#383838',
+        flame: '#e4572e',
+        glitter: '#eae8ff',
+        midnightBlue: '#282a36',
+        snow: '#fffafa',
     },
     typography: {
-        fontFamily: "'Open Sans', sans-serif",
+        canon: {
+            tag: 'h1',
+            fontSize: [10, 11, 12],
+            fontWeight: 700,
+            lineHeight: ['32px', '36px', '56px'],
+            fontFamily: 'Quicksand, sans-serif',
+        },
+        trafalgar: {
+            tag: 'h1',
+            fontSize: [6, 9, 12],
+            lineHeight: ['24px', '28px', '40px'],
+            fontWeight: 500,
+            fontFamily: "'Open Sans', sans-serif",
+        },
+        body: {
+            tag: 'p',
+            fontSize: [3, 4, 5],
+            fontFamily: "'Open Sans', sans-serif",
+            lineHeight: ['20px', '22px', '24px'],
+        },
     },
+    fontSizes: [12, 13, 14, 15, 16, 18, 20, 21, 22, 24, 26, 28, 32, 36, 52],
+    breakpoints: ['319px', '599px'],
 };
