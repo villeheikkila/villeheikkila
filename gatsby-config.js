717acd4e51f6e1f964aed7998a075c2eeebac287
Ville Heikkilä <tehomail@gmail.com>
2020-04-04 14:28:29 +0300

    feat: clean up, upgrade and configure the gatsby-starter-config

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    siteMetadata: {
+        title: `Ville Heikkila's developer portfolio`,
+        description: `I'm a software developer working as software consultant at Taito United. I have experience in development with various JavaScript frameworks on both the frontend and the backend. I especially like to work with React, TypeScript and GraphQL`,
+        author: `@villeheikkila`,
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
+};
