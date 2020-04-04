ca07620f3044b0daee35145fd7f015a4b6a9fc18
Ville Heikkilä <tehomail@gmail.com>
2020-04-04 15:48:52 +0300

    feat: implement styled-components and theme provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,12 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
+import styled from 'styled-components';
 
 const IndexPage = () => (
     <Layout>
         <SEO title="Home" />
-        <h1>Hi people</h1>
+        <Header>Hi people</Header>
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
         <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
@@ -17,5 +18,10 @@ const IndexPage = () => (
         <Link to="/page-2/">Go to page 2</Link>
     </Layout>
 );
+
+const Header = styled.h1`
+    font-weight: 'bold';
+    color: ${(props) => props.theme.colors.black};
+`;
 
 export default IndexPage;
---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+export default {
+    colors: {
+        black: '#000000',
+        white: '#FFFFFF',
+    },
+    typography: {
+        fontFamily: "'Open Sans', sans-serif",
+    },
+};
