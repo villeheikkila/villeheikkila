5616636860ea5fb1ad5b4d2404500c28940a7ed5
Ville Heikkilä <tehomail@gmail.com>
2020-04-04 18:10:46 +0300

    fix: add global css to fix viewport size

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
+import './src/global.css';
 
 export const wrapRootElement = ({ element }) => {
     return <ThemeProvider theme={theme}>{element}</ThemeProvider>;
